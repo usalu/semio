c51063bade09d7f7a6404c9ba6b8d97958e8aeba
Ueli Saluz <usaluz@posteo.de>
2023-01-30 14:34:39 +0100

    Prepare moving grpc code generation out of ui into .Net package

--- src/packages/csharp/Semio/Program.cs
+++ src/packages/csharp/Semio/Program.cs
@@ -1,4 +1,4 @@
-using Semio.Gateway.Services;
+//using Semio.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,14 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc().AddJsonTranscoding();
-builder.Services.AddGrpcReflection();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GatewayService>();
-app.MapGrpcReflectionService();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
