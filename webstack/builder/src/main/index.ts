5f5039aa3a6196b530a846063e696fbe4655a9be
Ueli Saluz <usaluz@posteo.de>
2024-03-06 19:12:41 +0100

    🔁Selection refactor⚒️Editor scaffold
    Builder:
    ➕Node and Attraction Editor scaffold
    🔁Selection now works different. Unfortunetly there is no other way but hacking to onSelect to enable different EditModes. Otherwise I would have to start to fork react-digraph.
    ➕Added IDraft interface that extends IGraphInput
    ➕Added save command
    🎨Added remaining old style to command bar results
    ➖Removed stroked hover effect on pieces because unrealiable
    ➖Disabled default menu in Electron
    ➕Install instruction in README

--- webstack/builder/src/main/index.ts
+++ webstack/builder/src/main/index.ts
@@ -2,82 +2,114 @@ import { app, shell, BrowserWindow, ipcMain, IpcMainEvent, dialog } from 'electr
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { writeFile } from 'fs'
 
 function createWindow(): void {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 900,
+        height: 670,
+        show: false,
+        autoHideMenuBar: true,
+        ...(process.platform === 'linux' ? { icon } : {}),
+        webPreferences: {
+            preload: join(__dirname, '../preload/index.js'),
+            sandbox: false
+        }
+    })
+
+    // Disable the default menu
+    mainWindow.setMenu(null);
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    mainWindow.webContents.setWindowOpenHandler((details) => {
+        shell.openExternal(details.url)
+        return { action: 'deny' }
+    })
+
+    // HMR for renderer base on electron-vite cli.
+    // Load the remote URL for development or the local html file for production.
+    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+        mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    } else {
+        mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
     }
-  })
-
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
-
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-  }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+        optimizer.watchWindowShortcuts(window)
+    })
 
+    ipcMain.on('select-directory', (ipcEvent: IpcMainEvent) => {
+        dialog
+            .showOpenDialog({
+                properties: ['openDirectory']
+            })
+            .then((result) => {
+                if (!result.canceled) {
+                    ipcEvent.reply('directory-selected', result.filePaths[0])
+                }
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    })
 
-  ipcMain.on('select-directory', (ipcEvent: IpcMainEvent) => {
-    dialog.showOpenDialog({
-      properties: ['openDirectory']
-    }).then(result => {
-      if (!result.canceled) {
-        ipcEvent.reply('directory-selected', result.filePaths[0]);
-      }
-    }).catch(err => {
-      console.error(err);
-    });
-  });
+    ipcMain.on(
+        'save-draft',
+        (ipcEvent: IpcMainEvent, draftJson: string, defaultName: string = 'formation') => {
+            dialog
+                .showSaveDialog({
+                    title: 'Save Draft',
+                    defaultPath: defaultName + '.draft',
+                    filters: [{ name: 'DRAFT', extensions: ['draft'] }]
+                })
+                .then((result) => {
+                    if (!result.canceled && result.filePath) {
+                        writeFile(result.filePath, draftJson, (err) => {
+                            if (err) {
+                                console.error(err)
+                            } else {
+                                ipcEvent.reply('draft-saved', result.filePath)
+                            }
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        }
+    )
 
-  createWindow()
+    createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 // In this file you can include the rest of your app"s specific main process
