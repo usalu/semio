4f9ae1a595fe21edf9fa2bf097aef5f3c7fb9f6a
Ueli Saluz <usaluz@posteo.de>
2024-03-14 23:36:37 +0100

    ⚒️Sketchpad ui base🖱️DragNDrop preparation🎨Control over theme
    Sketchpad:
    🔗dnd-kit
    🔗mui icons
    📐General segmentation of ui
    🖱️Drag and Drop skeleton for simply droping types and formations on the diagram editor
    🎨Disable (almost) all automatic theming from ant. Otherwise it was hard to control the theme because individual tokens were seeding different colors.

--- webstack/sketchpad/tailwind.config.js
+++ webstack/sketchpad/tailwind.config.js
@@ -5,7 +5,10 @@ export default {
             primary: '#ff344f',
             secondary: '#34d1bf',
             tertiary: '#fa9500',
-            dark: '#002430',
+            dark: '#001117',
+            darkGrey: '#001821',
+            grey: '#29383c',
+            lightGrey: '#cecbbd',
             light: '#f7f3e3',
             danger: '#a60009',
             warning: '#fccf05',
@@ -19,7 +22,10 @@ export default {
             fontFamily: {
                 noto: ['Noto Emoji', 'Anta', 'sans-serif'],
                 anta: ['Anta', 'Verdana', 'sans-serif']
-            }
+            },
+            borderWidth: {
+                'thin': '0.5px',
+              },
         }
     },
     plugins: []
