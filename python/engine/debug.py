89cdbc436d1a5f9f261b70e8e955c5ffa4c56418
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2024-08-16 20:58:14 +0200

    further try

---
+++ python/engine/debug.py
@@ -0,0 +1,198 @@
+import json
+from functools import lru_cache
+from os import remove
+from pathlib import Path
+from typing import List, Optional, Union
+from urllib.parse import urlparse
+from sqlmodel import (
+    SQLModel,
+    Field as ModelField,
+    Relationship,
+    Column,
+)
+from sqlalchemy import (
+    CheckConstraint,
+    UniqueConstraint,
+    String as SQLString,
+    Integer as SQLInteger,
+    create_engine,
+)
+from sqlalchemy.orm import (
+    sessionmaker,
+    Session,
+    validates,
+    object_session,
+    mapped_column,
+    Mapped,
+)
+from pydantic import BaseModel, Field, computed_field
+from fastapi import FastAPI
+from graphene import (
+    Schema,
+    ObjectType,
+    Field as GraphField,
+    NonNull as GraphNonNull,
+    String as GraphString,
+    List as GraphList,
+)
+import graphene
+from graphene_pydantic import PydanticInputObjectType, PydanticObjectType
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from uvicorn import run
+from starlette.applications import Starlette
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+HOST = "127.0.0.1"
+PORT = 5052
+
+NAME_LENGTH_MAX = 100
+
+
+@lru_cache(maxsize=100)
+def getLocalSession(path: str) -> Session:
+    engine = create_engine("sqlite:///" + path, echo=True)
+    SQLModel.metadata.create_all(engine)
+    return sessionmaker(bind=engine)()
+
+
+class A(SQLModel, table=True):
+    id: Optional[int] = ModelField(default=None, primary_key=True)
+    foo: str
+    bId: Optional[int] = ModelField(default=None, foreign_key="b.id")
+    b: Union["B", None] = Relationship(back_populates="a_s")
+
+
+class ANode(SQLAlchemyObjectType):
+    class Meta:
+        model = A
+
+    b = graphene.Field(lambda: BNode)
+
+    def resolve_b(self, info):
+        return self.b
+
+
+class AInput(PydanticInputObjectType):
+    class Meta:
+        model = A
+
+
+class B(SQLModel, table=True):
+    id: Optional[int] = ModelField(default=None, primary_key=True)
+    bar: str
+    # a: list[A] = ModelField(default_factory=list)
+    a_s: List[A] = Relationship(back_populates="b")
+
+
+class BNode(SQLAlchemyObjectType):
+
+    class Meta:
+        model = B
+
+    a_s = graphene.List(ANode)
+
+    def resolve_a_s(self, info):
+        return self.a_s
+
+
+class BInput(PydanticInputObjectType):
+
+    class Meta:
+        model = B
+
+
+class CreateB(graphene.Mutation):
+    class Arguments:
+        b = GraphNonNull(BInput)
+
+    b = GraphField(BNode)
+
+    def mutate(self, info, directory, b: BInput):
+        session = getLocalSession("engine2.sqlite3")
+        b = B(**b.dict())
+        session.add(b)
+        session.commit()
+        return CreateB(b=b)
+
+
+class C(SQLModel):
+    # _hidden_prop: Mapped[int] = mapped_column(primary_key=True)
+    # _hidden_prop: Optional[int] = ModelField(default=None, primary_key=True)
+    vis_prop: str
+
+
+class CInput(PydanticInputObjectType):
+    class Meta:
+        model = C
+
+
+class CNode(PydanticObjectType):
+    class Meta:
+        model = C
+
+
+# b = B(a=[A(foo="bar")])
+# try:
+#     remove("engine2.sqlite3")
+# except:
+#     pass
+# session = getLocalSession("engine2.sqlite3")
+# session.add(b)
+# session.commit()
+# pass
+
+
+def getAllBs():
+    # session = getLocalSession("engine2.sqlite3")
+    # types = session.query(B).all()
+    # types_dict = [type.model_dump() for type in types]
+    # return types_dict
+    # return types
+    a1 = A(foo="bar")
+    a2 = A(foo="baz")
+    b = B(bar="ham", a_s=[a1, a2])
+    return [b]
+
+
+class Query(ObjectType):
+    bs = GraphField(GraphList(BNode))
+    cs = GraphField(GraphList(CNode))
+
+    def resolve_bs(self, info):
+        return getAllBs()
+
+    def resolve_cs(self, info):
+        return [C(visible="foo"), C(visible="bar")]
+
+
+class Mutation(ObjectType):
+    create_b = CreateB.Field()
+
+
+schema = Schema(
+    query=Query,
+    mutation=Mutation,
+)
+with open("schema.graphql", "w", encoding="utf-8") as f:
+    f.write(str(schema))
+
+fastapi_app = FastAPI()
+
+
+@fastapi_app.get("/types")
+async def read_root() -> list[B]:
+    return getAllBs()
+
+
+engine = Starlette()
+engine.mount("/graphql", GraphQLApp(schema, on_get=make_graphiql_handler()))
+engine.mount("/", fastapi_app)
+
+run(
+    engine,
+    host=HOST,
+    port=PORT,
+    log_level="info",
+    access_log=False,
+    log_config=None,
+)
--- python/engine/semio.py
+++ python/engine/semio.py
@@ -1,19 +1,41 @@
 from functools import lru_cache
+from os import remove
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Union
 from urllib.parse import urlparse
 from sqlmodel import (
     SQLModel,
-    Field as SQLField,
+    Field as ModelField,
     Relationship,
+    Column,
 )
 from sqlalchemy import (
     CheckConstraint,
     UniqueConstraint,
     String as SQLString,
+    Integer as SQLInteger,
     create_engine,
 )
-from sqlalchemy.orm import sessionmaker, Session, validates
+from sqlalchemy.orm import sessionmaker, Session, validates, object_session
+from pydantic import computed_field
+from fastapi import FastAPI
+from graphene import (
+    Schema,
+    ObjectType,
+    Field as GraphField,
+    NonNull as GraphNonNull,
+    String as GraphString,
+    List as GraphList,
+)
+import graphene
+from graphene_pydantic import PydanticInputObjectType, PydanticObjectType
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from uvicorn import run
+from starlette.applications import Starlette
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+HOST = "127.0.0.1"
+PORT = 5052
 
 NAME_LENGTH_MAX = 100
 
@@ -25,82 +47,230 @@ def getLocalSession(path: str) -> Session:
     return sessionmaker(bind=engine)()
 
 
-class TagModel(SQLModel, table=True):
+class ArtifactModel(SQLModel):
+
+    name: str
+    description: str = ""
+    icon: str = ""
+
+
+class Tag(SQLModel, table=True):
     """🏷️ A tag is meta-data for grouping representations."""
 
     __tablename__ = "tag"
 
-    value: str = SQLField(
-        primary_key=True,
+    value: str = ModelField(primary_key=True)
+    representationPk: int = ModelField(
+        foreign_key=("representation.id"), primary_key=True, exclude=True
     )
-    representationId: int = SQLField(
-        foreign_key=("representation.id"), primary_key=True
-    )
-    representation: "RepresentationModel" = Relationship(back_populates="_tags")
+    representation: "Representation" = Relationship(back_populates="_tags")
 
 
-class RepresentationModel(SQLModel, table=True):
+class RepresentationBase(SQLModel):
     """💾 A representation is a link to a file that describes a type for a unique combination of level of detail, tags and mime."""
 
-    __tablename__ = "representation"
-    id: Optional[int] = SQLField(default=None, primary_key=True, exclude=True)
-    localId: str = SQLField(alias="lid")
     url: str
-    _tags: List[TagModel] = Relationship(
+    lod: str = ""
+
+
+class Representation(RepresentationBase, table=True):
+
+    __tablename__ = "representation"
+
+    pk: Optional[int] = ModelField(
+        sa_column=Column(
+            "id",
+            SQLInteger,
+            primary_key=True,
+        ),
+        default=None,
+        exclude=True,
+    )
+    id: str = ModelField(
+        sa_column=Column(
+            "localId",
+            SQLString(NAME_LENGTH_MAX),
+        )
+    )
+
+    _tags: list[Tag] = Relationship(
         back_populates="representation", cascade_delete=True
     )
-    typeId: Optional[int] = SQLField(default=None, foreign_key=("type.id"))
-    type: "TypeModel" = Relationship(back_populates="representations")
+    typePk: Optional[int] = ModelField(default=None, foreign_key=("type.id"))
+    type: Union["Type", None] = Relationship(back_populates="representations")
     __table_args__ = (UniqueConstraint("url"),)
 
     @property
-    def tags(self) -> List[str]:
+    def tags(self) -> list[str]:
         return [tag.value for tag in self._tags or []]
 
     @tags.setter
-    def tags(self, tags: List[str]):
-        self._tags = [TagModel(value=tag) for tag in tags]
+    def tags(self, tags: list[str]):
+        self._tags = [Tag(value=tag) for tag in tags]
 
     @validates("url")
     def validate_url(self, key: str, url: str):
         return url
 
 
-class TypeModel(SQLModel, table=True):
+class RepresentationOutput(RepresentationBase):
+
+    class Config:
+        title = "Representation"
+
+    id: str = ""
+    tags: list[str] = ModelField(default_factory=list)
+
+
+class RepresentationNode(PydanticObjectType):
+
+    class Meta:
+        model = Representation
+        name = "Representation"
+
+
+class RepresentationInput(PydanticInputObjectType):
+
+    class Meta:
+        model = Representation
+
+
+class TypeBase(ArtifactModel):
     """🧩 A type is a reusable element that can be connected with other types over ports."""
+
+    variant: str = ""
+    # __table_args__ = (UniqueConstraint("name", "variant", "kitId"),)
+
+
+class Type(TypeBase, table=True):
 
     __tablename__ = "type"
 
-    id: Optional[int] = SQLField(default=None, primary_key=True)
-    representations: List[RepresentationModel] = Relationship(
-        back_populates="type", cascade_delete=True
+    pk: Optional[int] = ModelField(
+        sa_column=Column(
+            "id",
+            SQLInteger,
+            primary_key=True,
+        ),
+        default=None,
+        exclude=True,
+    )
+    group: str = ModelField(
+        "", sa_column=Column("group_name", SQLString(NAME_LENGTH_MAX))
+    )
+    representations: list[Representation] = Relationship(
+        back_populates="type",
+        cascade_delete=True,
     )
 
-    # __table_args__ = (UniqueConstraint("name", "variant", "kitId"),)
 
-    def __repr__(self) -> str:
-        return f"Type(id={self.id!r}, name={self.name}, description={self.description}, icon={self.icon}, variant={self.variant} unit={self.unit}, kitId={self.kitId!r}, representations={self.representations!r}, ports={self.ports!r}, qualities={self.qualities!r}, pieces={self.pieces!r})"
+class TypeOutput(TypeBase):
 
-    def __str__(self) -> str:
-        return f"Type(id={str(self.id)}, kitId={str(self.kitId)})"
+    class Config:
+        title = "Type"
 
-    def client__str__(self) -> str:
-        return f"Type(name={self.name}, variant={self.variant})"
+    representations: list[RepresentationOutput] = ModelField(default_factory=list)
+
+
+class TypeNode(PydanticObjectType):
+    class Meta:
+        model = Type
+        name = "Type"
+
+
+class TypeInput(PydanticInputObjectType):
+    class Meta:
+        model = Type
+
+
+class CreateType(graphene.Mutation):
+    class Arguments:
+        type = GraphNonNull(TypeInput)
+
+    type = GraphField(TypeNode)
+
+    def mutate(self, info, directory, type: TypeInput):
+        session = getLocalSession("engine2.sqlite3")
+        type = Type(**type.dict())
+        session.add(type)
+        session.commit()
+        return CreateType(type=type)
 
 
 def create_db_and_tables():
-    r1 = RepresentationModel(localId="g", url="https://www.google.com")
-    r2 = RepresentationModel(localId="y", url="https://www.yahoo.com")
+    path = Path("engine2.sqlite3")
+    try:
+        remove(path)
+    except:
+        pass
+    r1 = Representation(id="g", url="https://www.google.com")
+    r2 = Representation(id="y", url="https://www.yahoo.com")
     r2.tags = ["tag1", "tag2"]
-    r3 = RepresentationModel(localId="y2", url="https://www.yahoo.com1")
-    t1 = TypeModel()
+    r2.tags = ["tag3", "tag4"]
+    r3 = Representation(id="y2", url="https://www.yahoo.com1")
+    t1 = Type(name="capsule")
     t1.representations = [r1, r2, r3]
-    engine = create_engine("sqlite:///test.sqlite3")
+    engine = create_engine("sqlite:///" + str(path))
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
         session.add(t1)
+        [r1n, r2n, r3n] = t1.representations
+        r2n.tags = ["tag5", "tag6"]
         session.commit()
         pass
+    pass
 
 
-# create_db_and_tables()
+create_db_and_tables()
+
+
+def getAllTypes():
+    session = getLocalSession("engine2.sqlite3")
+    types = session.query(Type).all()
+    # types_dict = [type.model_dump() for type in types]
+    # return types_dict
+    return types
+
+
+class TypesResponse(ObjectType):
+    types = GraphList(TypeNode)
+
+
+class Query(ObjectType):
+    types = GraphField(TypesResponse)
+
+    def resolve_types(self, info):
+        return TypesResponse(types=getAllTypes())
+
+
+class Mutation(ObjectType):
+    create_type = CreateType.Field()
+
+
+schema = Schema(
+    query=Query,
+    mutation=Mutation,
+)
+with open("schema.graphql", "w", encoding="utf-8") as f:
+    f.write(str(schema))
+
+fastapi_app = FastAPI()
+
+
+@fastapi_app.get("/types")
+async def read_root() -> list[TypeOutput]:
+    return getAllTypes()
+
+
+engine = Starlette()
+engine.mount("/graphql", GraphQLApp(schema, on_get=make_graphiql_handler()))
+engine.mount("/", fastapi_app)
+
+run(
+    engine,
+    host=HOST,
+    port=PORT,
+    log_level="info",
+    access_log=False,
+    log_config=None,
+)
