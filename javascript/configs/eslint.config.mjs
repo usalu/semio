4e24d4e52a305e14b3589a91111757d3bd867c1e
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-06 16:12:54 +0100

    added configs

---
+++ javascript/configs/eslint.config.mjs
@@ -0,0 +1,4 @@
+const eslintConfig = [
+];
+
+export default eslintConfig;
---
+++ javascript/configs/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff344f',
+        secondary: '#34d1bf',
+        tertiary: '#fa9500',
+        dark: '#001117',
+        darkGrey: '#001821',
+        grey: '#29383c',
+        lightGrey: '#cecbbd',
+        light: '#f7f3e3',
+        danger: '#a60009',
+        warning: '#fccf05',
+        info: '#dbbea1',
+        success: '#7eb77f',
+      },
+      borderWidth: {
+        'thin': '0.5px',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
