477afcf26293db8cecfb773695df17ac1899edb5
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-07 21:15:40 +0100

    adding deps and removed old code

---
+++ javascript/docs/gatsby-config.ts
@@ -0,0 +1,18 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+    // siteMetadata: {
+    //     siteUrl: `https://www.yourdomain.tld`,
+    // },
+    // graphqlTypegen: true,
+    // plugins: [
+    //     {
+    //         resolve: `gatsby-plugin-compile-es6-packages`,
+    //         options: {
+    //             modules: [`@repo/ui`],
+    //         },
+    //     },
+    // ],
+};
+
+export default config;
