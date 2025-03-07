ec88e50dc428bad63df6d70746659b4b6e175501
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-07 21:33:28 +0100

    more configs

--- javascript/configs/tailwind.config.ts
+++ javascript/configs/tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
---
+++ javascript/docs/eslintrc.js
@@ -0,0 +1,5 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: ["@semio/configs/eslint.config.mjs"],
+    ignorePatterns: ["gatsby-types.d.ts"],
+};
