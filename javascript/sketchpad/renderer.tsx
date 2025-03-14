c050984031ffd5db08c8d0a711d436a0b1d6eac3
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-14 03:52:49 +0100

    going back to original config and trying again...

--- javascript/sketchpad/renderer.tsx
+++ javascript/sketchpad/renderer.tsx
@@ -1,19 +1,19 @@
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 
-import Diagram from '@semio/core/components/ui/Diagram'
+// import Diagram from '@semio/core/components/ui/Diagram'
 
-function App() {
-    return (
-        <Diagram />
-    );
-}
+// function App() {
+//     return (
+//         <Diagram />
+//     );
+// }
 
-export default App;
+// export default App;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+// )
--- javascript/sketchpad/vite.renderer.config.mts
+++ javascript/sketchpad/vite.renderer.config.mts
@@ -1,9 +1,15 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(
-    { jsxRuntime: 'classic' }
-  )],
 })
+
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react(
+//         { jsxRuntime: 'classic' }
+//     )],
+// })
