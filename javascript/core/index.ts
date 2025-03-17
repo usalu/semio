8ed50e1b27334cc8a87a1d2858961d7895f0767e
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-17 10:21:44 +0100

    added exports and imports and started cleaning up config files

--- javascript/core/index.ts
+++ javascript/core/index.ts
@@ -1,1 +1,6 @@
 export { default as Diagram } from '@semio/core/components/ui/Diagram';
+export { default as eslintConfig } from '@semio/core/eslint.config';
+export { default as postcssConfig } from '@semio/core/postcss.config';
+export { default as tailwindConfig } from '@semio/core/tailwind.config';
+export { default as viteConfig } from '@semio/core/vite.config';
+export { default as vitestConfig } from '@semio/core/vitest.workspace';
