3e973ff907fdbffb82ef63ae3d27d1a9063b5801
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-03-17 10:37:31 +0100

    new config

--- javascript/core/tailwind.config.ts
+++ javascript/core/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./**/*.{ts,tsx,mdx}"],
+  content: ["./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
