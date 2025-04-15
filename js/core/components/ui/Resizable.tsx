07240ad3656e21f73679393ae04c5cf3ee8df054
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-04-15 10:17:45 +0200

    hover is now consistent with handle

--- js/core/components/ui/Resizable.tsx
+++ js/core/components/ui/Resizable.tsx
@@ -37,19 +37,21 @@ function ResizableHandle({
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
-        "relative flex w-px items-center justify-center transition-colors",
+        "relative flex w-px items-center justify-center",
         "border-r border-border hover:border-primary",
+        "before:absolute before:inset-y-0 before:-left-2 before:w-4 before:cursor-ew-resize", // Wider hit area
         "focus-visible:ring-ring focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden",
         "after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2",
         "data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full",
         "data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
+        "data-[panel-group-direction=vertical]:before:inset-x-0 data-[panel-group-direction=vertical]:before:-top-2 data-[panel-group-direction=vertical]:before:h-4 data-[panel-group-direction=vertical]:before:w-full data-[panel-group-direction=vertical]:before:cursor-ns-resize",
         "[&[data-panel-group-direction=vertical]>div]:rotate-90",
         className
       )}
       {...props}
     >
       {withHandle && (
-        <div className="z-10 flex h-4 w-3 items-center justify-center border border-border bg-background transition-colors group-hover:border-primary hover:border-primary hover:bg-primary">
+        <div className="z-10 flex h-4 w-3 items-center justify-center border border-border bg-background hover:border-primary hover:bg-primary">
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
