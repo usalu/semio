7fc1cce255e86e979b84ce1ed2bf59ef457c0f9e
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-04-16 23:00:13 +0200

    started  edges in diagram

--- js/core/components/ui/Diagram.tsx
+++ js/core/components/ui/Diagram.tsx
@@ -92,19 +92,6 @@ const ConnectionEdgeComponent: React.FC<EdgeProps<ConnectionEdge>> = ({
     sourceHandleId,
     targetHandleId,
 }) => {
-    const { getNode } = useReactFlow();
-
-    // const sourceHandle = getNode(source)?.handles.find((handle) => handle.id === sourceHandleId);
-    // const targetHandle = getNode(target)?.handles.find((handle) => handle.id === targetHandleId);
-
-    // const centerX = (sourceX + targetX) / 2;
-    // const centerY = (sourceY + targetY) / 2;
-    // unit vector pointing to center
-    // const length = Math.sqrt((centerX) ** 2 + (centerY) ** 2);
-    // const x = (centerX / length) * ICON_WIDTH;
-    // const y = (centerY / length) * ICON_WIDTH;
-
-    // const path = `M ${sourceX} ${sourceY} C ${sourceX + x} ${sourceY + y}, ${targetX - x} ${targetY - y}, ${targetX} ${targetY}`;
     const HANDLE_HEIGHT = 5;
     const path = `M ${sourceX} ${sourceY + HANDLE_HEIGHT / 2} L ${targetX} ${targetY + + HANDLE_HEIGHT / 2}`;
     return <BaseEdge path={path} />;
@@ -152,206 +139,30 @@ const DiagramCore: FC<DiagramProps> = ({ fullscreen, onPanelDoubleClick, design 
         id: 'diagram',
     });
 
-    const nodes = design.pieces?.map((piece) => ({
+    const initialNodes: PieceNode[] = design.pieces?.map((piece) => ({
         type: 'piece',
-        id: piece.id_,
-        position: { x: piece.center.x * 100, y: -piece.center.y * 100 },
+        id: piece.id_ || '',
+        position: { x: piece.center?.x || 0, y: piece.center?.y || 0 },
         data: { piece },
-    }));
+    })) || [];
 
-    const edges = design.connections?.map((connection) => ({
+    const initialEdges: ConnectionEdge[] = design.connections?.map((connection) => ({
         type: 'connection',
-        id: `${connection.connecting.piece.id_} -- ${connection.connected.piece.id_}`,
-        source: connection.connecting.piece.id_,
-        target: connection.connected.piece.id_,
-    }));
-
-    // const edges = design.connections?.map((connection) => ({
-    //     type: 'connection',
-    //     id: connection.id_,
-    //     source: connection.source.id_,
-    //     target: connection.target.id_,
-    // }));
-
-
-    // const types: Type[] = [
-    //     { name: 'base', ports: [{ id_: 't', t: 0, point: { x: 0, y: 0, z: 0 }, direction: { x: 1, y: 0, z: 0 } }] },
-    //     { name: 'tambour', ports: [{ id_: 't', t: 0, point: { x: 0, y: 0, z: 0 }, direction: { x: 1, y: 0, z: 0 } }] },
-    // ];
-    // const initialNodes: PieceNode[] = [
-    //     { type: 'piece', id: 'b', position: { x: 0, y: 100 }, data: { piece: { id_: 'b', type: { name: "base" } } } },
-    //     { type: 'piece', id: 't', position: { x: 0, y: 0 }, data: { piece: { id_: 't', type: { name: "tambour" } } } },
-    // ];
-    // const initialEdges: ConnectionEdge[] = [{ type: 'connection', id: 'base:top -- bottom:tambour', source: 'b', sourceHandle: 't', target: 't', targetHandle: 'sw' }];
-
-    // const setPresence = usePresenceSetter()
-    // const presenceMap = usePresence();
-
-    // const viewport = useViewport();
-
-    // const onUpdateCursor = (event) => {
-    //     if (event === null || event === undefined) {
-    //         return;
-    //     }
-    //     // TODO: Figure out how to get the x and y of the mouse pointer in the viewport coordinate system
-    //     const x = Math.round((event.clientX) - viewport.x);
-    //     const y = Math.round((event.clientY) - viewport.y);
-    //     // console.log(`mX: ${event.clientX}, mY: ${event.clientY}`);
-    //     // console.log(`vX: ${viewport.x}, vY: ${viewport.y}, z: ${viewport.zoom}`);
-    //     setPresence({
-    //         cursor: { x, y },
-    //     });
-    // }
+        id: `${connection.connecting.piece.id_ || ''} -- ${connection.connected.piece.id_ || ''}`,
+        source: connection.connecting.piece.id_ || '',
+        target: connection.connected.piece.id_ || '',
+    })) || [];
 
     const nodeTypes = useMemo(() => ({ piece: PieceNodeComponent }), []);
     const edgeTypes = useMemo(() => ({ connection: ConnectionEdgeComponent }), []);
 
-    // const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-    // const onConnect = useCallback(
-    //     (params: any) => setEdges((eds) => addEdge(params, eds)),
-    //     [setEdges],
-    // );
-
-    const MIN_DISTANCE = 100;
-    // const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    // const { getInternalNode } = useReactFlow();
-
-    // const onConnect = useCallback(
-    //     (params) => setEdges((eds) => addEdge(params, eds)),
-    //     [setEdges],
-    // );
-
-    // const getClosestEdge = useCallback((node) => {
-    //     const { nodeLookup } = store.getState();
-    //     const internalNode = getInternalNode(node.id);
-
-    //     const closestNode = Array.from(nodeLookup.values()).reduce(
-    //         (res, n) => {
-    //             if (n.id !== internalNode.id) {
-    //                 const dx =
-    //                     n.internals.positionAbsolute.x -
-    //                     internalNode.internals.positionAbsolute.x;
-    //                 const dy =
-    //                     n.internals.positionAbsolute.y -
-    //                     internalNode.internals.positionAbsolute.y;
-    //                 const d = Math.sqrt(dx * dx + dy * dy);
-
-    //                 if (d < res.distance && d < MIN_DISTANCE) {
-    //                     res.distance = d;
-    //                     res.node = n;
-    //                 }
-    //             }
-
-    //             return res;
-    //         },
-    //         {
-    //             distance: Number.MAX_VALUE,
-    //             node: null,
-    //         },
-    //     );
-
-    //     if (!closestNode.node) {
-    //         return null;
-    //     }
-
-    //     // '// Find the closest source and target handles
-    //     // let closestInternalHandle = null;
-    //     // let closestClosestHandle = null;
-    //     // let minHandleDistance = MIN_DISTANCE + ICON_WIDTH;
-
-    //     // console.log('closestNode', internalNode['type'], closestNode.node['type']);
-    //     // const internalHandles = nodeTypes[].handles || [];
-    //     // const closestHandles = nodeTypes[closestNode['type']].handles || [];
-
-    //     // internalHandles.forEach((sourceHandle) => {
-    //     //     closestHandles.forEach((targetHandle) => {
-    //     //         const dx =
-    //     //             targetHandle.positionAbsolute.x -
-    //     //             sourceHandle.positionAbsolute.x;
-    //     //         const dy =
-    //     //             targetHandle.positionAbsolute.y -
-    //     //             sourceHandle.positionAbsolute.y;
-    //     //         const handleDistance = Math.sqrt(dx * dx + dy * dy);
-    //     //         console.log('handleDistance', handleDistance, sourceHandle.id, targetHandle.id);
-
-    //     //         if (handleDistance < minHandleDistance) {
-    //     //             minHandleDistance = handleDistance;
-    //     //             closestInternalHandle = sourceHandle;
-    //     //             closestClosestHandle = targetHandle;
-    //     //         }
-    //     //     });
-    //     // });
-
-    //     // if (!closestInternalHandle || !closestClosestHandle) {
-    //     //     return null;
-    //     // }
-
-    //     const closeNodeIsSource =
-    //         closestNode.node.internals.positionAbsolute.x <
-    //         internalNode.internals.positionAbsolute.x;
-
-    //     return {
-    //         id: closeNodeIsSource
-    //             ? `${closestNode.node.id}-${node.id}`
-    //             : `${node.id}-${closestNode.node.id}`,
-    //         type: 'connection',
-    //         source: closeNodeIsSource ? closestNode.node.id : node.id,
-    //         // sourceHandle: closeNodeIsSource ? closestInternalHandle.id : closestClosestHandle.id,
-    //         target: closeNodeIsSource ? node.id : closestNode.node.id,
-    //         // targetHandle: closeNodeIsSource ? closestClosestHandle.id : closestInternalHandle.id,
-    //     };
-    // }, []);
-
-    // const onNodeDrag = useCallback(
-    //     (_, node) => {
-    //         const closeEdge = getClosestEdge(node);
-
-    //         setEdges((es) => {
-    //             const nextEdges = es.filter((e) => e.className !== 'temp');
-
-    //             if (
-    //                 closeEdge &&
-    //                 !nextEdges.find(
-    //                     (ne) =>
-    //                         ne.source === closeEdge.source && ne.target === closeEdge.target,
-    //                 )
-    //             ) {
-    //                 closeEdge.className = 'temp';
-    //                 nextEdges.push(closeEdge);
-    //             }
-
-    //             return nextEdges;
-    //         });
-    //     },
-    //     [getClosestEdge, setEdges],
-    // );
-
-    // const onNodeDragStop = useCallback(
-    //     (_, node) => {
-    //         const closeEdge = getClosestEdge(node);
-
-    //         setEdges((es) => {
-    //             const nextEdges = es.filter((e) => e.className !== 'temp');
-
-    //             if (
-    //                 closeEdge &&
-    //                 !nextEdges.find(
-    //                     (ne) =>
-    //                         ne.source === closeEdge.source && ne.target === closeEdge.target,
-    //                 )
-    //             ) {
-    //                 nextEdges.push(closeEdge);
-    //             }
-
-    //             return nextEdges;
-    //         });
-    //     },
-    //     [getClosestEdge],
-    // );
-
+    const onConnect = useCallback(
+        (params: any) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
 
     return (
         <ReactFlow
@@ -362,14 +173,11 @@ const DiagramCore: FC<DiagramProps> = ({ fullscreen, onPanelDoubleClick, design 
             edgeTypes={edgeTypes}
             connectionMode={ConnectionMode.Loose}
             elementsSelectable
-            // fitView
             minZoom={0.1}
             maxZoom={5}
-            // onNodesChange={onNodesChange}
-            // onEdgesChange={onEdgesChange}
-            // onNodeDrag={onNodeDrag}
-            // onNodeDragStop={onNodeDragStop}
-            // onConnect={onConnect}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
             zoomOnDoubleClick={false}
             onDoubleClickCapture={(e) => {
                 e.stopPropagation();
