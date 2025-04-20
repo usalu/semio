72b86e181f77eddbdbdeaf3084c6cc1e41d6ebf2
KinanSarak <kinan.sarak@gmail.com>
2025-04-20 15:36:07 +0200

    edited intro pages

--- js/docs/astro.config.ts
+++ js/docs/astro.config.ts
@@ -49,6 +49,7 @@ export default defineConfig({
                             'core-concepts',
                         ],
                     },
+                    'toolkit',
                     'installation',
                     'starter'
                 ],
