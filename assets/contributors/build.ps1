a762a0d1143826506a73df68da7411b91ac96b73
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-04-24 14:27:39 +0200

    added active powershell and python file to debug options

--- assets/contributors/build.ps1
+++ assets/contributors/build.ps1
@@ -1,25 +1,14 @@
-. ..\..\powershell.ps1
+. ..\..\..\powershell.ps1
 
-$resolution = 90
-
-# read contributors from contributors.txt
+$resolutions = @(90, 400)
 $contributors = Get-Content -Path "..\lists\contributors.txt"
-
-# create temp folder
-$tempFolder = ".\temp"
-if (-not (Test-Path -Path $tempFolder)) {
-    New-Item -ItemType Directory -Path $tempFolder
-}
-
-# download avatars from https://github.com/ACCOUNT.png and save to .\temp
 foreach ($contributor in $contributors) {
     $url = "https://github.com/$contributor.png"
-    $destination = Join-Path -Path $tempFolder -ChildPath "$contributor.png"
-    Invoke-WebRequest -Uri $url -OutFile $destination
-}
-
-# resize avatars to $resolution and crop to circle and save .\ACCOUNT.png
-foreach ($contributor in $contributors) {
-    
-    
+    Invoke-WebRequest -Uri $url -OutFile "$contributor.png"
+    foreach ($resolution in $resolutions) {
+        $image = Get-Image -Path "$contributor.png"
+        $image.Resize($resolution, $resolution)
+        $image.Crop(0, 0, $resolution, $resolution)
+        $image.Save("$contributor_round_$resolution.png")
+    }
 }
