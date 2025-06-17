e2946d3feef5ce500ccf2fac06d3e0bcac40a359
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-06-17 13:05:49 +0200

    added highlight

--- js/core/components/ui/GrasshopperCatalogue.tsx
+++ js/core/components/ui/GrasshopperCatalogue.tsx
@@ -7,17 +7,18 @@ interface ParamProps {
     description: string;
     kind: string;
     extended?: boolean;
-    setDescription?: (description: string) => void;
+    highlight?: boolean;
+    setHighlight?: (name: string, description: string) => void;
 }
 
-const Param: FC<ParamProps> = ({ name, nickname, description, kind, extended, setDescription }) => {
+const Param: FC<ParamProps> = ({ name, nickname, description, kind, extended, highlight, setHighlight }) => {
     return (
         <div
-            className="size-fit"
+            className={`size-fit ${extended && highlight ? 'bg-primary' : ''}`}
             onClick={(e) => {
-                if (extended && setDescription) {
+                if (extended && setHighlight) {
                     e.stopPropagation();
-                    setDescription(description);
+                    setHighlight(name, description);
                 }
             }}
         >
@@ -28,47 +29,58 @@ const Param: FC<ParamProps> = ({ name, nickname, description, kind, extended, se
 
 
 interface ParamsProps {
-    params?: ParamProps[];
-    input?: boolean;
-    setDescription?: (description: string) => void;
+    params: ParamProps[];
+    input: boolean;
+    highlight: number | undefined;
+    setHighlight: (highlight: Highlight) => void;
     extended?: boolean;
 }
 
-const Params: FC<ParamsProps> = ({ params, input, setDescription, extended }) => {
+const Params: FC<ParamsProps> = ({ params, input, highlight, setHighlight, extended }) => {
+    const setHighlightHandler = (name: string, description: string) => {
+        setHighlight({ input, index: params.findIndex(param => param.name === name), description })
+    };
     return (
-        <div className="flex flex-col gap-1"
+        <div className="flex flex-col"
         >
             {params.map((param, index) => (
-                <Param key={index} {...param} extended={extended} setDescription={setDescription} />
+                <Param key={index} {...param} extended={extended} setHighlight={setHighlightHandler} highlight={highlight === index} />
             ))}
         </div >
     );
 }
 
+interface Highlight {
+    input: boolean;
+    index: number;
+    description: string;
+}
+
 interface GrasshopperComponentProps {
     name: string;
     nickname: string;
+    description: string;
     inputs?: ParamProps[];
     outputs?: ParamProps[];
 }
 
-const GrasshopperComponent: FC<GrasshopperComponentProps> = ({ name, nickname, inputs, outputs }) => {
-    const [description, setDescription] = useState('A description of the component can go here…');
+const GrasshopperComponent: FC<GrasshopperComponentProps> = ({ name, nickname, description, inputs, outputs }) => {
+    const [highlight, setHighlight] = useState<Highlight | undefined>(undefined);
     const [extended, setExtended] = useState(false);
     return (
         <div id="extended-component" className=""
-            onDoubleClick={
+            onClick={
                 () => setExtended(!extended)
             }>
             <div id="component" className="flex flex-row items-stretch size-fit border-2 border-dark bg-light justify-between relative">
-                <Params params={inputs || []} setDescription={setDescription} extended={extended} />
-                <div className="flex items-center justify-center min-w-10 bg-dark text-light text-lg font-bold" style={{ writingMode: 'vertical-rl' }}>
-                    <p className="m-0 p-0 w-full text-center rotate-180">{extended ? name : nickname}</p>
+                {inputs && <Params params={inputs || []} input={true} setHighlight={setHighlight} extended={extended} highlight={highlight?.input ? highlight?.index : undefined} />}
+                <div className="flex items-center justify-center bg-dark text-light text-lg font-bold" style={{ writingMode: 'vertical-rl' }}>
+                    <p className="w-full text-center rotate-180">{extended ? name : nickname}</p>
                 </div>
-                <Params params={outputs || []} input={false} setDescription={setDescription} extended={extended} />
+                {outputs && <Params params={outputs || []} input={false} setHighlight={setHighlight} extended={extended} highlight={highlight?.input ? undefined : highlight?.index} />}
                 {extended &&
-                    <div id="description" className="bg-gray w-full absolute left-0 top-full mt-1 p-2">
-                        {description}
+                    <div id="description" className="bg-gray w-full absolute left-0 top-full mt-1 p-1">
+                        {highlight?.description}
                     </div>
                 }
             </div>
