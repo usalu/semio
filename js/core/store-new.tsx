41d2652c5060ce9d25ec8634f9269048ca78d814
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-08-15 13:35:16 +0200

    added subscription mechanism, more hooks, ...

--- js/core/store-new.tsx
+++ js/core/store-new.tsx
@@ -19,8 +19,7 @@
 
 // #endregion
 
-import React, { useContext, useMemo } from "react";
-import { createContext } from "vm";
+import React, { createContext, useContext, useMemo } from "react";
 import {
   Author,
   Camera,
@@ -77,6 +76,9 @@ export enum Layout {
 
 // #region Api
 
+type Subscribe = () => void;
+type Unsubscribe = () => void;
+
 interface QualityState {
   quality: Quality;
 }
@@ -85,7 +87,13 @@ interface QualityActions {
   update: (diff: QualityDiff) => void;
 }
 
-interface QualityStore extends QualityState, QualityActions {}
+interface QualitySubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface QualityStore extends QualityState, QualityActions, QualitySubscriptions {}
 
 interface AuthorState {
   author: Author;
@@ -95,7 +103,13 @@ interface AuthorActions {
   update: (diff: Partial<Author>) => void;
 }
 
-interface AuthorStore extends AuthorState, AuthorActions {}
+interface AuthorSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface AuthorStore extends AuthorState, AuthorActions, AuthorSubscriptions {}
 
 interface LocationState {
   location: Location;
@@ -105,7 +119,13 @@ interface LocationActions {
   update: (diff: Partial<Location>) => void;
 }
 
-interface LocationStore extends LocationState, LocationActions {}
+interface LocationSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface LocationStore extends LocationState, LocationActions, LocationSubscriptions {}
 
 interface RepresentationState {
   representation: Representation;
@@ -125,11 +145,27 @@ interface RepresentationActions {
   };
 }
 
+interface RepresentationSubscriptions {
+  on: {
+    created: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      representation: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface RepresentationChildStores {
   qualities: Map<QualityId, QualityStore>;
 }
 
-interface RepresentationStore extends RepresentationState, RepresentationActions, RepresentationChildStores {}
+interface RepresentationStore extends RepresentationState, RepresentationActions, RepresentationSubscriptions, RepresentationChildStores {}
 
 interface DiagramPointState {
   diagramPoint: DiagramPoint;
@@ -139,7 +175,13 @@ interface DiagramPointActions {
   update: (diff: Partial<DiagramPoint>) => void;
 }
 
-interface DiagramPointStore extends DiagramPointState, DiagramPointActions {}
+interface DiagramPointSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface DiagramPointStore extends DiagramPointState, DiagramPointActions, DiagramPointSubscriptions {}
 
 interface PointState {
   point: Point;
@@ -149,7 +191,13 @@ interface PointActions {
   update: (diff: Partial<Point>) => void;
 }
 
-interface PointStore extends PointState, PointActions {}
+interface PointSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface PointStore extends PointState, PointActions, PointSubscriptions {}
 
 interface VectorState {
   vector: Vector;
@@ -159,7 +207,13 @@ interface VectorActions {
   update: (diff: Partial<Vector>) => void;
 }
 
-interface VectorStore extends VectorState, VectorActions {}
+interface VectorSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface VectorStore extends VectorState, VectorActions, VectorSubscriptions {}
 
 interface PlaneState {
   plane: Plane;
@@ -169,7 +223,13 @@ interface PlaneActions {
   update: (diff: Partial<Plane>) => void;
 }
 
-interface PlaneStore extends PlaneState, PlaneActions {}
+interface PlaneSubscriptions {
+  on: {
+    updated: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+  };
+}
+
+interface PlaneStore extends PlaneState, PlaneActions, PlaneSubscriptions {}
 interface PortState {
   port: Port;
 }
@@ -186,10 +246,28 @@ interface PortActions {
     qualities: (qualityIds: QualityId[]) => void;
   };
 }
+
+interface PortSubscriptions {
+  on: {
+    created: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      port: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface PortChildStores {
   qualities: Map<QualityId, QualityStore>;
 }
-interface PortStore extends PortState, PortActions, PortChildStores {}
+
+interface PortStore extends PortState, PortActions, PortSubscriptions, PortChildStores {}
 
 interface TypeState {
   type: Type;
@@ -219,13 +297,43 @@ interface TypeActions {
     qualities: (ids: QualityId[]) => void;
   };
 }
+
+interface TypeSubscriptions {
+  on: {
+    created: {
+      representation: (subscribe: Subscribe) => Unsubscribe;
+      representations: (subscribe: Subscribe) => Unsubscribe;
+      port: (subscribe: Subscribe) => Unsubscribe;
+      ports: (subscribe: Subscribe) => Unsubscribe;
+      author: (subscribe: Subscribe) => Unsubscribe;
+      authors: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      type: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      representation: (subscribe: Subscribe) => Unsubscribe;
+      representations: (subscribe: Subscribe) => Unsubscribe;
+      port: (subscribe: Subscribe) => Unsubscribe;
+      ports: (subscribe: Subscribe) => Unsubscribe;
+      author: (subscribe: Subscribe) => Unsubscribe;
+      authors: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface TypeChildStores {
   representations: Map<RepresentationId, RepresentationStore>;
   ports: Map<PortId, PortStore>;
   authors: Map<string, AuthorStore>;
   qualities: Map<QualityId, QualityStore>;
 }
-interface TypeStore extends TypeState, TypeActions, TypeChildStores {}
+
+interface TypeStore extends TypeState, TypeActions, TypeSubscriptions, TypeChildStores {}
 
 interface PieceState {
   piece: Piece;
@@ -245,11 +353,27 @@ interface PieceActions {
   };
 }
 
+interface PieceSubscriptions {
+  on: {
+    created: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      piece: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface PieceChildStores {
   qualities: Map<QualityId, QualityStore>;
 }
 
-interface PieceStore extends PieceState, PieceActions, PieceChildStores {}
+interface PieceStore extends PieceState, PieceActions, PieceSubscriptions, PieceChildStores {}
 
 interface ConnectionState {
   connection: Connection;
@@ -269,11 +393,27 @@ interface ConnectionActions {
   };
 }
 
+interface ConnectionSubscriptions {
+  on: {
+    created: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      connection: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface ConnectionChildStores {
   qualities: Map<QualityId, QualityStore>;
 }
 
-interface ConnectionStore extends ConnectionState, ConnectionActions, ConnectionChildStores {}
+interface ConnectionStore extends ConnectionState, ConnectionActions, ConnectionSubscriptions, ConnectionChildStores {}
 
 interface DesignState {
   design: Design;
@@ -305,6 +445,34 @@ interface DesignActions {
   };
 }
 
+interface DesignSubscriptions {
+  on: {
+    created: {
+      piece: (subscribe: Subscribe) => Unsubscribe;
+      pieces: (subscribe: Subscribe) => Unsubscribe;
+      connection: (subscribe: Subscribe) => Unsubscribe;
+      connections: (subscribe: Subscribe) => Unsubscribe;
+      author: (subscribe: Subscribe) => Unsubscribe;
+      authors: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      design: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      piece: (subscribe: Subscribe) => Unsubscribe;
+      pieces: (subscribe: Subscribe) => Unsubscribe;
+      connection: (subscribe: Subscribe) => Unsubscribe;
+      connections: (subscribe: Subscribe) => Unsubscribe;
+      author: (subscribe: Subscribe) => Unsubscribe;
+      authors: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface DesignChildStores {
   pieces: Map<PieceId, PieceStore>;
   connections: Map<ConnectionId, ConnectionStore>;
@@ -312,7 +480,7 @@ interface DesignChildStores {
   qualities: Map<QualityId, QualityStore>;
 }
 
-interface DesignStore extends DesignState, DesignActions, DesignChildStores {}
+interface DesignStore extends DesignState, DesignActions, DesignSubscriptions, DesignChildStores {}
 
 interface KitState {
   kit: Kit;
@@ -340,13 +508,37 @@ interface KitActions {
   };
 }
 
+interface KitSubscriptions {
+  on: {
+    created: {
+      type: (subscribe: Subscribe) => Unsubscribe;
+      types: (subscribe: Subscribe) => Unsubscribe;
+      design: (subscribe: Subscribe) => Unsubscribe;
+      designs: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      kit: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      type: (subscribe: Subscribe) => Unsubscribe;
+      types: (subscribe: Subscribe) => Unsubscribe;
+      design: (subscribe: Subscribe) => Unsubscribe;
+      designs: (subscribe: Subscribe) => Unsubscribe;
+      quality: (subscribe: Subscribe) => Unsubscribe;
+      qualities: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
 interface KitChildStores {
   types: Map<TypeId, TypeStore>;
   designs: Map<DesignId, DesignStore>;
   qualities: Map<QualityId, QualityStore>;
 }
 
-interface KitStore extends KitState, KitActions, KitChildStores {}
+interface KitStore extends KitState, KitActions, KitSubscriptions, KitChildStores {}
 
 export interface DesignEditorSelection {
   selectedPieceIds: PieceId[];
@@ -376,6 +568,8 @@ interface DesignEditorState {
   isTransactionActive: boolean;
   presence: DesignEditorPresence;
   others: DesignEditorPresenceOther[];
+  diff: KitDiff;
+  diffs: KitDiff[];
 }
 
 interface DesignEditorActions {
@@ -387,9 +581,32 @@ interface DesignEditorActions {
     presence: (presence: DesignEditorPresence) => void;
     others: (others: DesignEditorPresenceOther[]) => void;
   };
+  transaction: {
+    start: () => void;
+    abort: () => void;
+    finalize: () => void;
+  };
 }
 
-interface DesignEditorStore extends DesignEditorState, DesignEditorActions {}
+interface DesignEditorSubscriptions {
+  on: {
+    undone: (subscribe: Subscribe) => Unsubscribe;
+    redone: (subscribe: Subscribe) => Unsubscribe;
+    set: {
+      fullscreenPanel: (subscribe: Subscribe) => Unsubscribe;
+      selection: (subscribe: Subscribe) => Unsubscribe;
+      presence: (subscribe: Subscribe) => Unsubscribe;
+      others: (subscribe: Subscribe) => Unsubscribe;
+    };
+    transaction: {
+      started: (subscribe: Subscribe) => Unsubscribe;
+      aborted: (subscribe: Subscribe) => Unsubscribe;
+      finalized: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
+}
+
+interface DesignEditorStore extends DesignEditorState, DesignEditorActions, DesignEditorSubscriptions {}
 
 interface SketchpadState {
   mode: Mode;
@@ -422,14 +639,44 @@ interface SketchpadActions {
   };
 }
 
-interface SketchpadChildStores {
-  kits: Map<KitId, KitStore>;
-  designEditors: Map<string, DesignEditorStore>;
+interface SketchpadSubscriptions {
+  on: {
+    created: {
+      kit: (subscribe: Subscribe) => Unsubscribe;
+      kits: (subscribe: Subscribe) => Unsubscribe;
+      designEditor: (subscribe: Subscribe) => Unsubscribe;
+      designEditors: (subscribe: Subscribe) => Unsubscribe;
+    };
+    updated: {
+      kit: (subscribe: Subscribe, deep?: boolean) => Unsubscribe;
+    };
+    deleted: {
+      kit: (subscribe: Subscribe) => Unsubscribe;
+      kits: (subscribe: Subscribe) => Unsubscribe;
+      designEditor: (subscribe: Subscribe) => Unsubscribe;
+      designEditors: (subscribe: Subscribe) => Unsubscribe;
+    };
+    set: {
+      mode: (subscribe: Subscribe) => Unsubscribe;
+      theme: (subscribe: Subscribe) => Unsubscribe;
+      layout: (subscribe: Subscribe) => Unsubscribe;
+      activeDesignEditorId: (subscribe: Subscribe) => Unsubscribe;
+    };
+  };
 }
 
-interface SketchpadStore extends SketchpadState, SketchpadActions, SketchpadChildStores {}
+interface SketchpadChildStores {
+  kits: Map<KitId, KitStore>;
+  designEditors: Map<DesignId, DesignEditorStore>;
+}
+
+interface SketchpadStore extends SketchpadState, SketchpadActions, SketchpadSubscriptions, SketchpadChildStores {}
 
 // #endregion Api
+
+class SketchpadStore {}
+
+// #region Hooks
 
 // #region Scoping
 
@@ -510,9 +757,23 @@ const useDesignEditorScope = () => useContext(DesignEditorScopeContext);
 
 // #endregion Scoping
 
-// #region Hooks
+export function useSketchpad<T>(selector?: (sketchpad: SketchpadStore, id?: string) => T): T {
+  throw new Error("Not implemented");
+}
+
+export function useDesignEditor<T>(selector?: (editor: DesignEditorStore, designId: DesignId) => T, id?: string): T {
+  throw new Error("Not implemented");
+}
+
+export function useDesignEditors<T>(selector?: (editors: Map<DesignId, DesignEditorStore>) => T): T {
+  throw new Error("Not implemented");
+}
 
 export function useKit<T>(selector?: (kit: KitStore) => T, id?: KitId): T {
+  throw new Error("Not implemented");
+}
+
+export function useKits<T>(selector?: (kits: Map<KitId, KitStore>) => T): T {
   throw new Error("Not implemented");
 }
 
@@ -520,7 +781,15 @@ export function useDesign<T>(selector?: (design: DesignStore) => T, kitId?: KitI
   throw new Error("Not implemented");
 }
 
+export function useDesigns<T>(selector?: (designs: Map<DesignId, DesignStore>) => T, kitId?: KitId): T {
+  throw new Error("Not implemented");
+}
+
 export function useType<T>(selector?: (type: TypeStore) => T, kitId?: KitId, typeId?: TypeId): T {
+  throw new Error("Not implemented");
+}
+
+export function useTypes<T>(selector?: (types: Map<TypeId, TypeStore>) => T, kitId?: KitId): T {
   throw new Error("Not implemented");
 }
 
@@ -528,7 +797,15 @@ export function usePiece<T>(selector?: (piece: PieceStore) => T, kitId?: KitId, 
   throw new Error("Not implemented");
 }
 
+export function usePieces<T>(selector?: (pieces: Map<PieceId, PieceStore>) => T, kitId?: KitId, designId?: DesignId): T {
+  throw new Error("Not implemented");
+}
+
 export function useConnection<T>(selector?: (connection: ConnectionStore) => T, kitId?: KitId, designId?: DesignId, connectionId?: ConnectionId): T {
+  throw new Error("Not implemented");
+}
+
+export function useConnections<T>(selector?: (connections: Map<ConnectionId, ConnectionStore>) => T, kitId?: KitId, designId?: DesignId): T {
   throw new Error("Not implemented");
 }
 
@@ -536,7 +813,15 @@ export function usePort<T>(selector?: (port: PortStore) => T, kitId?: KitId, typ
   throw new Error("Not implemented");
 }
 
+export function usePorts<T>(selector?: (ports: Map<PortId, PortStore>) => T, kitId?: KitId, typeId?: TypeId): T {
+  throw new Error("Not implemented");
+}
+
 export function useRepresentation<T>(selector?: (representation: RepresentationStore) => T, kitId?: KitId, typeId?: TypeId, representationId?: RepresentationId): T {
+  throw new Error("Not implemented");
+}
+
+export function useRepresentations<T>(selector?: (representations: Map<RepresentationId, RepresentationStore>) => T, kitId?: KitId, typeId?: TypeId): T {
   throw new Error("Not implemented");
 }
 
@@ -544,15 +829,15 @@ export function useQuality<T>(selector?: (quality: QualityStore) => T, parentId?
   throw new Error("Not implemented");
 }
 
+export function useQualities<T>(selector?: (qualities: Map<QualityId, QualityStore>) => T, parentId?: string): T {
+  throw new Error("Not implemented");
+}
+
 export function useAuthor<T>(selector?: (author: AuthorStore) => T, parentId?: string, authorId?: string): T {
   throw new Error("Not implemented");
 }
 
-export function useDesignEditor<T>(selector?: (editor: DesignEditorStore) => T, id?: string): T {
-  throw new Error("Not implemented");
-}
-
-export function useSketchpad<T>(selector?: (sketchpad: SketchpadStore) => T): T {
+export function useAuthors<T>(selector?: (authors: Map<string, AuthorStore>) => T, parentId?: string): T {
   throw new Error("Not implemented");
 }
 
