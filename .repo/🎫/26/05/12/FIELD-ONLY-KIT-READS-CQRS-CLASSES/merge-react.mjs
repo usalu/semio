07080cfcb26794f8af433c40f3681a71d6080505
Ueli Saluz <ueli@semio-tech.com>
2026-05-12 12:00:44 +0200

    213

---
+++ .repo/🎫/26/05/12/FIELD-ONLY-KIT-READS-CQRS-CLASSES/merge-react.mjs
@@ -0,0 +1,16 @@
+import fs from "node:fs";
+
+const idx = fs.readFileSync("semio/react/index.tsx", "utf8");
+const sp = fs.readFileSync("semio/react/sketchpad-runtime.tsx", "utf8");
+
+const spStart = sp.indexOf("const {\n  applyKitClientSnapshotToLocalStore");
+if (spStart < 0) throw new Error("sketchpad const block not found");
+const spBody = sp.slice(spStart);
+
+const idxOut = idx.replace(
+  /\/\/#region 📎SketchpadRuntime\nexport \* from "\.\/sketchpad-runtime\.js";\n\/\/#endregion 📎SketchpadRuntime\n/,
+  `//#region 🪁SketchpadHost\n${spBody}\n//#endregion 🪁SketchpadHost\n`,
+);
+
+fs.writeFileSync("semio/react/index.tsx", idxOut, "utf8");
+console.log("merged react bytes", idxOut.length);
